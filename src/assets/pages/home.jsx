25188892fdaa0a2ad8f68ad658266b0ee1d8d3ca
fndeveloper <fasihnasir60@gmail.com>
2025-01-02 19:40:57 +0500

    first commit

---
+++ src/assets/pages/home.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    {/* CAROUSEL START */}
+      <div
+  id="carouselExampleIndicators"
+  className="carousel slide carousel-fade"
+  data-bs-ride="carousel"
+>
+  <div className="carousel-indicators">
+    <button
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide-to={0}
+      className="active dd"
+      aria-current="true"
+      aria-label="Slide 1"
+    />
+    <button
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide-to={1}
+      aria-label="Slide 2"
+      className='dd'
+    />
+
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+        <div className="i2 d-flex flex-lg-row flex-column justify-content-center align-items-center">
+<div className="col-lg-5 col-12 ps-4">
+    <h5 className='col-yellow fw-bold '>FAST FOOD BURGERS</h5>
+    <h1 className='display-1 text-white fw-bold head1'>BEST <br /> BURGERS</h1>
+<p className='text-white text-capitalize fs-5'>secret family recipes are the stuff of legends and stack <br /> burger blend is no exception</p>
+<button className='btn1  col-lg-3' >Order Now
+<i className="fas fa-shipping-fast span-hov d-flex flex-row align-items-center justify-content-center"></i>
+</button>
+</div>
+<div className="col-lg-7 col-12 i3">
+
+</div>
+
+        </div>
+
+    </div>
+    <div className="carousel-item">
+    <div className="i1 d-flex flex-lg-row flex-column justify-content-center align-items-center">
+
+<div className="col-lg-5 col-12 ps-4">
+    <h5 className='col-yellow fw-bold '>FAST FOOD BURGERS</h5>
+    <h1 className='display-2 text-white fw-bold head1'>BEEF BURGERS</h1>
+<p className='text-white text-capitalize fs-5'>secret family recipes are the stuff of legends and stack <br /> burger blend is no exception</p>
+<button className='btn1  col-lg-3' >Order Now
+<i className="fas fa-shipping-fast span-hov d-flex flex-row align-items-center justify-content-center"></i>
+</button>
+</div>
+<div className="col-lg-7 col-12 i4">
+
+</div>
+    </div>
+    </div>
+
+  </div>
+  {/* <button
+    className="carousel-control-prev"
+    type="button"
+    data-bs-target="#carouselExampleIndicators"
+    data-bs-slide="prev"
+  >
+    <span className="carousel-control-prev-icon" aria-hidden="true" />
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button
+    className="carousel-control-next"
+    type="button"
+    data-bs-target="#carouselExampleIndicators"
+    data-bs-slide="next"
+  >
+    <span className="carousel-control-next-icon" aria-hidden="true" />
+    <span className="visually-hidden">Next</span>
+  </button> */}
+</div>
+    {/* CAROUSEL END */}
+<div className="container-fluid">
+    <div className="col-lg-3  col-md-6 col-12">
+        
+    </div>
+    <div className="col-lg-3 col-md-6 col-12"></div>
+    <div className="col-lg-3 col-md-6 col-12"></div>
+    <div className="col-lg-3 col-md-6 col-12"></div>
+</div>
+    </>
+  )
+}
---
+++ src/assets/pages/navbar.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import logo from "../img/removelogo.png"
+export default function Navbar() {
+  return (
+    <div>
+    {/* TOP */}
+    <div className="animate__fadeIn container-fluid d-flex flex-row d-lg-flex d-none justify-content-center align-items-center tophead m-0 px-4  py-2 top">
+
+   <div className="col-4 d-flex  align-items-center">
+   <i className="fa-solid span-head fa-location-dot pe-2"></i>
+    <span className='span-head1'>Find a Store</span>
+    <i className="fa-solid span-head fa-cart-shopping ps-3"></i>
+  
+    <span className='span-head ps-2 span-head1'> Order tracking</span>
+   </div>
+   <div className="col-4 d-flex  align-items-center justify-content-center">
+ 
+    <span className='span-head1'>100% Secure delivery without contacting the courier    </span>
+
+  
+
+   </div>
+   <div className="col-4 d-flex justify-content-end  align-items-center">
+
+          <a href="#" class="text-white mx-2"><i class="fab fa-twitter fs-6"></i></a>
+          <a href="#" class="text-white mx-2"><i class="fab fa-facebook  fs-6"></i></a>
+          <a href="#" class="text-white mx-2"><i class="fab fa-instagram fs-6" ></i></a>
+          <a href="#" class="text-white mx-2"><i class="fab fa-linkedin  fs-6"></i></a>
+
+   </div>
+   
+ 
+</div>
+    {/* TOP */}
+{/* NAV */}
+    <nav>
+    <div className="animate__fadeIn container-fluid d-flex flex-row d-lg-flex d-none justify-content-center align-items-center tophead m-0 px-4   shadow-lg top1">
+
+   <div className="col-4 d-flex  align-items-center">
+   <i class="fa-solid fa-bars span-head fs-4" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></i>
+   
+   </div>
+   <div className="col-4 d-flex  align-items-center justify-content-center">
+ 
+  
+        <img src={logo} className='img-fluid col-4' alt="" />
+     
+
+  
+
+   </div>
+   <div className="col-4 d-flex justify-content-end  align-items-center">
+<button className='btn1 me-1'>Order Now
+<i className="fas fa-shipping-fast span-hov d-flex flex-row align-items-center justify-content-center"></i>
+</button>
+
+<button className='btn1'>
+<i className="fa-solid fa-cart-shopping fs-6 p-1  d-flex flex-row align-items-center justify-content-center"></i>
+
+
+<i className="fa-solid fa-cart-shopping span-hov d-flex flex-row align-items-center justify-content-center"></i>
+</button>
+
+
+
+   </div>
+   
+ 
+</div>
+    </nav>
+{/* NAV */}
+{/* offcanvas */}
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Try scrolling the rest of the page to see this option in action.</p>
+  </div>
+</div>
+{/* offcanvas */}
+    </div>
+  )
+}
